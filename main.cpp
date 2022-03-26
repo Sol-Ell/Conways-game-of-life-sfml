86b2130a208be430a83be05eb498255a5401d1d5
Jamshed <62467472+akhunpochoevjamshed@users.noreply.github.com>
2022-03-26 17:10:26 +0200

    Added ability to toggle pause/play by pressing P key

--- main.cpp
+++ main.cpp
@@ -6,15 +6,21 @@
 
 int main()
 {
-    const int CELL_SIZE = 10;
+    const int CELL_SIZE = 20;
     sf::Vector2f CELL_VECTOR(CELL_SIZE, CELL_SIZE);
-    const int GRID_WIDTH = 50;
-    const int GRID_HEIGHT = 50;
+
+    sf::RectangleShape cell;
+    cell.setSize(CELL_VECTOR);
+    cell.setOutlineThickness(0.5f);
+
+    const int GRID_WIDTH = 30;
+    const int GRID_HEIGHT = 30;
     const int LAST_COL = GRID_WIDTH - 1;
     const int LAST_ROW = GRID_HEIGHT - 1;
     const int GRID_N = GRID_WIDTH * GRID_WIDTH;
-    int GRID[GRID_N];
-    int NEXT_GRID[GRID_N];
+    
+    bool GRID[GRID_N]; // false - empty true - cell
+    bool NEXT_GRID[GRID_N];
 
     const int WIN_WIDTH = GRID_WIDTH * CELL_SIZE;
     const int WIN_HEIGHT = GRID_HEIGHT * CELL_SIZE;
@@ -27,9 +33,9 @@ int main()
         GRID[n] = rand() % 2;
     }
 
-    sf::RenderWindow window(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "Conway's Game of Life Implementation using C++ and SFML");
-    window.setFramerateLimit(50);
+    sf::RenderWindow window(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "Conway's Game of Life");
 
+    bool isPlaying = true;
     while (window.isOpen())
     {
         sf::Event event;
@@ -37,24 +43,37 @@ int main()
         {
             switch (event.type)
             {
-                case sf::Event::Closed:
-                {
+                case sf::Event::Closed: 
                     window.close();
-                    return 0;
-                }
+                    break;
+                case sf::Event::MouseButtonPressed: {
+                        int pressedX = event.mouseButton.x / CELL_SIZE;
+                        int pressedY = event.mouseButton.y / CELL_SIZE;
+                        if (!isPlaying && pressedX <= GRID_WIDTH * CELL_SIZE && pressedY <= GRID_HEIGHT * CELL_SIZE) {
+                            GRID[pressedY * GRID_WIDTH + pressedX] = !GRID[pressedY * GRID_WIDTH + pressedX];
+                        }
+                    }
+                    break;
+                case sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::P) {
+                        isPlaying = !isPlaying;
+                    }
+                    break;
             }
         }
         
         window.clear(WHITE);
-
         for(int y = 0; y < GRID_HEIGHT; y++)
         {
             for (int x = 0; x < GRID_WIDTH; x++) {
-                sf::RectangleShape cell;
-                cell.setSize(CELL_VECTOR);
                 cell.setPosition(x * CELL_SIZE, y * CELL_SIZE);
                 cell.setFillColor((GRID[y * GRID_WIDTH + x] == 1) ? BLACK : WHITE);
+                cell.setOutlineColor((GRID[y * GRID_WIDTH + x] == 1) ? WHITE : BLACK);
                 window.draw(cell);
+
+                if (!isPlaying) {
+                    continue;
+                }
 
                 // prepare next grid
                 int gridY = y * GRID_WIDTH;
@@ -102,53 +121,13 @@ int main()
                 }
             }
         }
-        for (int n = 0; n < GRID_N; n++) {
-            GRID[n] = NEXT_GRID[n];
+        if (isPlaying) {
+            for (int n = 0; n < GRID_N; n++) {
+                GRID[n] = NEXT_GRID[n];
+            }
         }
         window.display();
         sf::sleep(sf::milliseconds(delay));
     }
     return 0;
 }
-/*
-#include <stddef.h>
-#include <stdbool.h>
-
-void evolve(int GRID_HEIGHT, int GRID_WIDTH, bool GRID[GRID_HEIGHT][GRID_WIDTH], bool NEXT_GRID[GRID_HEIGHT][GRID_WIDTH])
-{
-    int LAST_ROW = GRID_HEIGHT-1, LAST_COL = GRID_WIDTH-1;
-    for (int y = 0; y < GRID_HEIGHT; y++) {
-        for (int x = 0; x < GRID_WIDTH; x++) {
-            int n = 0;
-            if (y != 0) {
-                if (x != 0) 
-                    n += GRID[y-1][x-1];
-                
-                n += GRID[y-1][x];
-                
-                if (x != LAST_COL) 
-                    n += GRID[y-1][x+1];
-            }
-            if (x != 0) 
-                n += GRID[y][x-1];
-            if (x != LAST_COL)
-                n += GRID[y][x+1];
-            
-            if (y != LAST_ROW) {
-                if (x != 0) 
-                    n += GRID[y+1][x-1];
-                
-                n += GRID[y+1][x];
-                
-                if (x != LAST_COL) 
-                    n += GRID[y+1][x+1];
-            }
-            if (n < 2 || n > 3) 
-                NEXT_GRID[y][x] = 0;
-            else if (n == 3)
-                NEXT_GRID[y][x] = 1;
-            else 
-                NEXT_GRID[y][x] = GRID[y][x];
-        }
-    }
-}*/
